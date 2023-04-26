7a6f79ed1fdff36b6fcffae18be006d616f9f559
4L302 <alegarg2002@gmail.com>
2023-04-26 15:35:25 +0200

    .

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<script>
+import axios from 'axios';
+import { store } from './data/store';
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    headerComponent,
+    characterList,
+    characterCard
+
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    
+  },
+  mounted() {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  header {
+  line-height: 1.5;
+}
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
